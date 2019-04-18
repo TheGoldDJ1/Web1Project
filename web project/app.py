3f9639a7059df49b44ec8436ed2177c626b94aa0
TheGoldDJ1 <38407675+TheGoldDJ1@users.noreply.github.com>
2019-04-18 20:58:51 -0300

    Add files via upload

---
+++ web project/app.py
@@ -0,0 +1,152 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+import json
+# import yaml
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scott:tiger@localhost/mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = yaml.load(open('db.yaml'))
+# app.config['MYSQL_HOST'] = db['mysql_host']
+# app.config['MYSQL_USER'] = db['mysql_user']
+# app.config['MYSQL_PASSWORD'] = db['mysql_password']
+# app.config['MYSQL_DB'] = db['mysql_db']
+
+# mysql = MySQL(app)
+db = SQLAlchemy(app)
+
+
+@app.before_first_request
+def setup():
+    db.Model.metadata.drop_all(bind=db.engine)
+    db.Model.metadata.create_all(bind=db.engine)
+
+# When the Flask app is shutting down, close the database session
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db.session.remove()
+
+from models import CodeName, ErrorSolution, ErrorTable
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+# ErrorTable table
+@app.route('/errortable', methods=['GET'])
+def show_all_errors():
+    print(request)
+    records = ErrorTable.query.all()
+    records = list(map(lambda x: x.toDict(), records))
+    response = jsonify(records)
+    response.status_code = 200
+    return response
+
+@app.route('/errortable', methods=['POST'])
+def save_errors():
+    if request.content_type  == 'application/json':
+        request_text = request.json
+        if 'username' in request_text:
+            print(request_text)
+            p = ErrorTable(request_text['username'])
+        else:
+            response = jsonify({'message': 'Invalid fields specified'})
+            response.status_code = 400
+            return response
+
+        if 'errorcode' in request_text:
+            p.errorcode = request_text['errorcode']
+
+        db.session.add(p)
+        db.session.commit()
+        response = jsonify({
+            'id': p.id,
+            'username': p.username,
+            'errorcode': p.errorcode
+        })
+        response.status_code = 201
+        return response
+    else:
+        response = jsonify({'message': 'Invalid format of data in the request'})
+        response.status_code = 400
+        return response
+
+# CodeName Table
+@app.route('/codename', methods=['GET'])
+def show_all_errorcodes():
+    print(request)
+    records = CodeName.query.all()
+    records = list(map(lambda x: x.toDict(), records))
+    response = jsonify(records)
+    response.status_code = 200
+    return response
+
+@app.route('/codename', methods=['POST'])
+def save_errorcodes():
+    if request.content_type  == 'application/json':
+        request_text = request.json
+        if 'errorcode' in request_text:
+            print(request_text)
+            p = CodeName(request_text['errorcode'], request_text['codename'])
+        else:
+            response = jsonify({'message': 'Invalid fields specified'})
+            response.status_code = 400
+            return response
+
+        # if 'codename' in request_text:
+        #     p.codename = request_text['codename']
+
+        db.session.add(p)
+        db.session.commit()
+        response = jsonify({
+            'errorcode': p.errorcode,
+            'codename': p.codename
+        })
+        response.status_code = 201
+        return response
+    else:
+        response = jsonify({'message': 'Invalid format of data in the request'})
+        response.status_code = 400
+        return response
+
+
+# ErrorSolution Table
+@app.route('/errorsolution', methods=['GET'])
+def show_all_errorsolutions():
+    print(request)
+    records = ErrorSolution.query.all()
+    records = list(map(lambda x: x.toDict(), records))
+    response = jsonify(records)
+    response.status_code = 200
+    return response
+
+@app.route('/errorsolution', methods=['POST'])
+def save_errorsolutions():
+    if request.content_type  == 'application/json':
+        request_text = request.json
+        if 'errorcode' in request_text:
+            print(request_text)
+            p = ErrorSolution(request_text['errorcode'], request_text['message'])
+        else:
+            response = jsonify({'message': 'Invalid fields specified'})
+            response.status_code = 400
+            return response
+
+        # if 'codename' in request_text:
+        #     p.codename = request_text['codename']
+
+        db.session.add(p)
+        db.session.commit()
+        response = jsonify({
+            'errorcode': p.errorcode,
+            'message': p.message
+        })
+        response.status_code = 201
+        return response
+    else:
+        response = jsonify({'message': 'Invalid format of data in the request'})
+        response.status_code = 400
+        return response
---
+++ web project/models.py
@@ -0,0 +1,65 @@
+from sqlalchemy import Integer, Column, String, DateTime, ForeignKey
+from sqlalchemy.sql import func
+
+from app import db
+
+
+
+class CodeName(db.Model):
+    __tablename__ = 'codename'
+    errorcode = Column(Integer, primary_key=True, nullable=False)
+    codename = Column(String(30))
+    errortables = db.relationship('ErrorTable', backref='codename', lazy='dynamic')
+
+    def __init__(self, errorcode, codename=None):
+        self.errorcode = errorcode
+        if codename:
+            self.codename = codename
+    
+    def toDict(self):
+       return {
+            'errorcode': self.errorcode,
+            'codename': self.codename
+        }
+
+class ErrorSolution(db.Model):
+    __tablename__ = 'errorsolution'
+    errorcode = Column(Integer, primary_key=True, nullable=False)
+    date = Column(DateTime(), server_default=func.now())
+    message = Column(String(256))
+
+    def __init__(self, errorcode, message=None):
+        self.errorcode = errorcode
+        if message:
+            self.message = message
+
+    def toDict(self):
+       return {
+            'errorcode': self.errorcode,
+            'date': self.date,
+            'message': self.message
+        }
+
+class ErrorTable(db.Model):
+    __tablename__ = "errortable"
+    id = Column(Integer, primary_key=True, autoincrement="auto")
+    username = Column(String(30), nullable=False)
+    errorcode = Column(Integer, ForeignKey('codename.errorcode'))
+    date = Column(DateTime(), server_default=func.now())
+    # posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __init__(self, username, errorcode=None):
+        self.username = username
+        if errorcode:
+            self.errorcode = errorcode
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.password}')"
+
+    def toDict(self):
+       return {
+            'id': self.id,
+            'username': self.username,
+            'errorcode': self.errorcode,
+            'date': self.date
+        }
